8e061291b3584b7fe5aa7eecdbdc3d6ef53d7de4
F0eiVer <arefev030303@mail.ru>
2022-10-23 15:02:57 +1000

    added classes

--- domain/Class1.cs
+++ domain/Class1.cs
@@ -9,5 +9,84 @@ using System.Text;
 
 namespace domain
 {
-    
+    public class Role
+    {
+        public uint ID;
+        Role(uint id)
+        {
+            ID = id;
+        }
+    }
+
+    public class Specialization
+    {
+        public uint ID;
+        public string name;
+
+        Specialization(uint id, string name)
+        {
+            ID = id;
+            this.name = name;
+        }
+    }
+
+    public class User
+    {
+        public uint ID;
+        public int phoneNumber;
+        public string fullName;
+        public Role role;
+
+        User(uint id, int phone, string FName, Role role)
+        {
+            ID = id;
+            phoneNumber = phone;
+            fullName = FName;
+            this.role = role;
+        }
+
+    }
+
+    public class Doctor
+    {
+        public uint ID;
+        public string fullName;
+        public Specialization specialization;
+
+        Doctor(uint id, string FName, Specialization specialization)
+        {
+            ID = id;
+            fullName = FName;
+            this.specialization = specialization;
+        }
+
+    }
+
+    public class Appointment
+    {
+        public string startTime, finishTime;
+        public uint patientID, doctorID;
+
+        Appointment(string STime, string FTime, User patient, Doctor doctor)
+        {
+            startTime = STime;
+            finishTime = FTime;
+            patientID = patient.ID;
+            doctorID = doctor.ID;
+        }
+
+    }
+
+    public class Timetable
+    {
+        public uint doctorID;
+        public string startTime, finishTime;
+
+        Timetable(string STime, string FTime, Doctor doctor)
+        {
+            startTime = STime;
+            finishTime = FTime;
+            doctorID = doctor.ID;
+        }
+    }
 }
