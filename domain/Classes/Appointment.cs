e2d8636c949f7222cc6d9d97435d0f755fae5678
F0eiVer <arefev030303@mail.ru>
2022-11-01 17:26:14 +1000

    Added classes

---
+++ domain/Classes/Appointment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain.Classes
+{
+    internal class Appointment
+    {
+        public string startTime, finishTime;
+        public uint patientID, doctorID;
+
+        Appointment(string STime, string FTime, User patient, Doctor doctor)
+        {
+            startTime = STime;
+            finishTime = FTime;
+            patientID = patient.ID;
+            doctorID = doctor.ID;
+        }
+    }
+}
---
+++ domain/Classes/Timetable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain.Classes
+{
+    internal class Timetable
+    {
+        public uint doctorID;
+        public string startTime, finishTime;
+
+        Timetable(string STime, string FTime, Doctor doctor)
+        {
+            startTime = STime;
+            finishTime = FTime;
+            doctorID = doctor.ID;
+        }
+    }
+}
