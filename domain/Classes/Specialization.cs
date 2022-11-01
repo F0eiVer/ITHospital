08b030b83639687ce647e6bbd693a9bb82c6823b
F0eiVer <arefev030303@mail.ru>
2022-11-01 17:51:51 +1000

    Added classes

---
+++ domain/Classes/Specialization.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain.Classes
+{
+    internal class Specialization
+    {
+        public uint ID;
+        public string name;
+
+        Specialization(uint id, string name)
+        {
+            ID = id;
+            this.name = name;
+        }
+    }
+}
---
+++ domain/Classes/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain.Classes
+{
+    internal class User
+    {
+        public uint ID;
+        public int phoneNumber;
+        public string fullName;
+        public Role role;
+
+        User(uint id, int phone, string FName, Role role)
+        {
+            ID = id;
+            phoneNumber = phone;
+            fullName = FName;
+            this.role = role;
+        }
+    }
+}
